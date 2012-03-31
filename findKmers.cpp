5429df4ba4179f859c0b3a69b40b66b77e4006ad
albertwcheng <albertwcheng@gmail.com>
2012-03-31 03:14:16 -0400

    commit

--- findKmers.cpp
+++ findKmers.cpp
@@ -1,3 +1,22 @@
+/***************************************************************************
+ Copyright 2012 Wu Albert Cheng <albertwcheng@gmail.com>
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ *******************************************************************************/ 
+
 #include <fstream>
 #include <string>
 #include <iostream>
@@ -553,6 +572,7 @@ int main(int argc,char**argv){
 	if(argc<5){ 
 		cerr<<"Usage:" <<argv[0]<<" <fgfilename> <bgfilename> <k> <howmanyToFind>"<<endl;
 		cerr<<"Description: find <howmanyToFind> top enriched <k>-mers from HT-SELEX experiments using <fgfilename> and <bgfilename> fastq files assuming there's only one motif per sequence"<<endl;
+		cerr<<"Specify <howmanyToFind>=0 to print all kmers"<<endl;
 		return 1;
 	}
 	
