78108063ca7940a80fcd4ff8ef87b8206bfcdbea
albertwcheng <albertwcheng@gmail.com>
2012-07-28 02:24:25 -0400

    commit

---
+++ DNAPrefixTree.h
@@ -0,0 +1,326 @@
+#include <queue>
+#include <stack>
+#include <string>
+#include <iostream>
+#include <inttypes.h>
+#include <list>
+using namespace std;
+
+typedef uint64_t uint64;
+
+#define MODE_FOREGROUND 'F'
+#define MODE_BACKGROUND 'B'
+
+template <class T>
+class SmartPtr
+{
+private:
+	T* ptr;
+public:
+	inline SmartPtr(T* _ptr=NULL):ptr(_ptr){}
+	
+	inline void operator=( T *_ptr)
+	{
+		ptr=_ptr;
+	}
+	
+	inline operator T*() const
+	{
+		return ptr;
+	}
+	
+	inline T& operator *() const
+	{
+		return *ptr;
+	}
+	
+	inline T* operator ->() const
+	{
+		return ptr;
+	}
+	
+	inline bool operator<(const SmartPtr<T>& right) const
+	{
+		return (**this)<(*right);
+	}
+	inline bool operator>(const SmartPtr<T>& right) const
+	{
+		return (**this)>(*right);
+	}
+	inline bool operator==(const SmartPtr<T>& right) const
+	{
+		return (**this)==(*right);
+	}
+	inline bool operator<=(const SmartPtr<T>& right) const
+	{
+		return !(**this>*right);
+	}
+	
+	inline bool operator>=(const SmartPtr<T>& right) const
+	{
+		return !(**this<*right);
+	}
+	
+	inline bool operator!=(const SmartPtr<T>& right) const
+	{
+		return !((**this)==(*right));
+	}
+	
+	inline bool ptrEquals(const SmartPtr<T>& right) const
+	{
+		return ptr==right.ptr;
+	}
+	
+};
+
+
+
+class DNATreeNode{
+	public:
+		DNATreeNode* parent;
+		
+		char thisLetter;
+		DNATreeNode* A_branch;
+		DNATreeNode* C_branch;
+		DNATreeNode* G_branch;
+		DNATreeNode* T_branch;
+		
+		
+		
+		
+		uint64 fgcount;
+		uint64 bgcount;
+		
+		double enrichment(double normalization=1.0) const{
+			if(bgcount<1){
+				return -1;
+			}	
+			return double(fgcount)/bgcount*normalization;	
+		}
+		
+		inline bool operator < (const DNATreeNode& right) const{
+			return this->enrichment()<right.enrichment();
+		}
+		
+		DNATreeNode(DNATreeNode* _parent=NULL,char _thisLetter='.'):parent(_parent),thisLetter(_thisLetter),A_branch(NULL),C_branch(NULL),G_branch(NULL),T_branch(NULL),fgcount(0),bgcount(0){}
+		
+		DNATreeNode* selectBranch(char c,bool create=false){
+			switch( c ){
+				case 'A':case 'a':
+					if(!A_branch && create){
+						A_branch=new DNATreeNode(this,'A');	
+					}
+					return A_branch;
+				case 'C':case 'c':
+					if(!C_branch  && create ){
+						C_branch=new DNATreeNode(this,'C');	
+					}
+					return C_branch;	
+				case 'G':case 'g':
+					if(!G_branch && create){
+						G_branch=new DNATreeNode(this,'G');	
+					}
+					return G_branch;
+				case 'T':case 't':
+					if(!T_branch && create){
+						T_branch=new DNATreeNode(this,'T');
+					}
+					return T_branch;
+				default:
+					return NULL;	
+			}	
+			
+			
+	
+		}
+		
+		
+		DNATreeNode* visitInc(char c,char mode){
+			
+			DNATreeNode* selectedBranch=selectBranch(c,true);
+				
+			if(selectedBranch){
+				if(mode==MODE_FOREGROUND){
+					selectedBranch->fgcount++;	
+				}else if(mode==MODE_BACKGROUND){
+					selectedBranch->bgcount++;
+				}	
+			}
+			
+			return selectedBranch;
+		}
+		
+		
+		
+		
+		DNATreeNode* getNodeByPath(const char*path,int length){
+			DNATreeNode* curNode=this;
+			for(int i=0;i<length;i++){
+				
+				curNode=curNode->selectBranch( path[i] );
+				if(curNode==NULL){
+					break;	
+				}
+			}	
+			
+			return curNode;
+		}
+		
+		string getPathFromRoot(){
+			DNATreeNode* curNode=this;
+			string path;
+			while(curNode){
+				if(curNode->thisLetter!='.')
+					path=curNode->thisLetter+path;
+				curNode=curNode->parent;	
+			}
+			
+			return path;	
+		}
+		
+		void putChildrenOntoQ(queue<DNATreeNode*> &Q){
+			if (A_branch) Q.push(A_branch);
+			if (C_branch) Q.push(C_branch);
+			if (G_branch) Q.push(G_branch);
+			if (T_branch) Q.push(T_branch);
+		}
+		
+		void putChildrenOntoS(stack<DNATreeNode*> &S){
+						
+			
+			if (T_branch) S.push(T_branch);
+			if (G_branch) S.push(G_branch);
+			if (C_branch) S.push(C_branch);
+			if (A_branch) S.push(A_branch);
+
+		}		
+		void freeSubtree(){
+			//DFS delete tree
+			queue<DNATreeNode*> Q;
+			putChildrenOntoQ(Q);
+			while(!Q.empty()){
+				DNATreeNode* v=Q.front();
+				Q.pop();
+				v->putChildrenOntoQ(Q);
+				//now delete v
+				delete v;	
+			}
+		}
+		
+		void printSubtree(){
+			stack<DNATreeNode*> S;
+			putChildrenOntoS(S);
+			while(!S.empty()){
+				DNATreeNode* v=S.top();
+				S.pop();
+				cout<<v->getPathFromRoot()<<"\t"<<v->fgcount<<"\t"<<v->bgcount<<endl;
+				v->putChildrenOntoS(S);	
+			}
+		}
+	
+};
+
+
+class DNATree{
+	public:
+		DNATreeNode *root;
+		DNATreeNode **tails; //keep the tails
+		int maxK;
+		
+		char countMode;
+		uint64 numNodes;
+		
+		list< SmartPtr<DNATreeNode> > *listsPerK;
+		
+		void setCountMode(char _countMode){
+			countMode=_countMode;	
+		}
+		
+		void addNodeToList(int depth,DNATreeNode* node){
+			listsPerK[depth-1].push_back(node);
+			numNodes++;
+		}
+		
+		
+		void sortLists(){
+			for(int i=0;i<maxK;i++){
+				listsPerK[i].sort();
+			}	
+		}
+		
+		void printLevelNodes(int level,double normalization){
+			for(list<SmartPtr<DNATreeNode> >::reverse_iterator i=listsPerK[level-1].rbegin();i!=listsPerK[level-1].rend();i++){
+				cout<<(*i)->getPathFromRoot()<<"\t"<<(*i)->fgcount<<"\t"<<(*i)->bgcount<<"\t"<<(*i)->enrichment(normalization)<<endl;	
+			}	
+		}
+		
+		
+		DNATree(int _maxK):maxK(_maxK),numNodes(0){
+			root=new DNATreeNode;
+			tails=new DNATreeNode*[maxK];
+			//init tails
+			for(int i=0;i<maxK;i++){
+				tails[i]=NULL;	
+			}
+			countMode=MODE_FOREGROUND;
+			listsPerK=new list<SmartPtr<DNATreeNode> >[maxK];
+		}
+		~DNATree(){
+			root->freeSubtree();
+			delete root;
+			delete[] tails;
+			delete[] listsPerK;
+			
+		}
+		
+		void feedSafe(char c){
+			int depth;
+			for(int i=1;i<maxK;i++){
+				//shift
+				if(!tails[i])
+					continue;
+				
+				tails[i-1]=tails[i]->visitInc( c, countMode);
+				depth=maxK-i+1;
+				if(tails[i-1]->fgcount+tails[i-1]->bgcount==1){
+					//new node
+					addNodeToList(depth,tails[i-1]);
+					
+				}
+				
+			}
+			
+			tails[maxK-1]=root->visitInc( c,countMode);
+			depth=1;
+			if(tails[maxK-1]->fgcount+tails[maxK-1]->bgcount==1){
+				addNodeToList(depth,tails[maxK-1]);
+				
+			}
+			
+		}
+		
+		void feed(char c){
+			switch( c){
+				case 'A':case 'a':case 'C':case 'c':case 'G':case 'g':case 'T':case 't':
+					//ok!
+					feedSafe( c);
+				break;
+				
+				default: //anything not matching, ignore and set all tails NULL;
+					for(int i=0;i<maxK;i++){
+						tails[i]=NULL;	
+					}
+			}
+		}
+		void feedSeq(const string& S){
+			const char* s=S.c_str();
+			int len=S.length();
+			for(int i=0;i<len;i++){
+				feed(s[i]);	
+			}
+		}
+		
+		void printTree(){
+			root->printSubtree();
+		}
+};
---
+++ DNAPrefixTreeTest.cpp
@@ -0,0 +1,139 @@
+#include "DNAPrefixTree.h"
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+class SimpleFastqReader
+{
+	
+public:
+	virtual bool onNewSeq(const string& name,const string& seq,const string& quals)=0;
+	
+	inline void readFile(const string& filename){
+		ifstream fin(filename.c_str());
+		string name;
+		string seq;
+		string quals;
+
+		uint64 reallino=0;
+		uint64 lino=0;
+		
+		bool continuetoread=true;
+		
+		while(continuetoread && fin.good()){
+			string line;
+			getline(fin,line); 
+			
+			lino++;
+			
+			
+			if(line.length()==0)
+				continue;
+			
+			reallino++;
+			
+			switch(reallino%4){
+				case 1: //a start of new seq
+					
+					if(line[0]!='@'){
+						cerr<<"Warning: improper Fastq formatting at "<<filename<<":line "<<lino<<"="<<line<<".Expecting @"<<endl;
+					}
+					
+					if(seq.length()>0){
+						continuetoread=this->onNewSeq(name,seq,quals);
+					}
+					
+					
+					name=line.substr(1); //to the end
+					seq="";
+					quals="";
+					
+
+					
+					
+					break;
+				case 2:
+					seq=line;
+					break;
+				case 3:
+					if(line[0]!='+'){
+						cerr<<"Warning: improper Fastq formatting at "<<filename<<":line "<<lino<<"="<<line<<".Expecting +"<<endl;
+					}
+					break;
+				case 0:
+					quals=line;
+					break;
+					
+				default:
+					break;
+					
+			}
+		}
+		
+		if(seq.length()>0){
+			this->onNewSeq(name,seq,quals);
+		}
+		
+		fin.close();
+	}
+	
+};
+
+
+class DNATreeKmerFinder:public SimpleFastqReader,public DNATree{
+	public:
+	uint64 numFgSeq;
+	uint64 numBgSeq;
+	DNATreeKmerFinder(int _maxK):DNATree(_maxK),numFgSeq(0),numBgSeq(0){}
+
+	void readForegroundFile(string filename){
+		DNATree::countMode=MODE_FOREGROUND;
+		readFile(filename);
+	}
+	
+	void readBackgroundFile(string filename){
+		DNATree::countMode=MODE_BACKGROUND;
+		readFile(filename);	
+	}
+	
+	bool onNewSeq(const string& name,const string& seq,const string& quals){
+		feedSeq(seq);
+		
+		if(DNATree::countMode==MODE_FOREGROUND){
+			numFgSeq++;
+			if(numFgSeq%100000==1){
+				cerr<<"processed "<<numFgSeq<<" fg sequences. numNodes="<<numNodes<<endl;
+			}	
+		}else{
+			numBgSeq++;	
+			if(numBgSeq%100000==1){
+				cerr<<"processed "<<numBgSeq<<" bg sequences. numNodes="<<numNodes<<endl;
+			}	
+		}
+		return true;
+	}
+};
+
+int main(int argc,const char **argv){
+	//DNATree tree(5);
+	//tree.feedSeq("ACGTAANC");
+	
+	//tree.feedSeq("ACGTCCCATGCCAT");
+	//tree.printTree();
+	
+	if(argc<3){
+		cerr<<"Usage:"<<argv[0]<<" fgfile bgfile"<<endl;
+		return 1;	
+	}
+	
+	DNATreeKmerFinder finder(10);
+	finder.readForegroundFile(argv[1]);
+	finder.readBackgroundFile(argv[2]);
+	//finder.printTree();
+	finder.sortLists();
+	finder.printLevelNodes(5,double(finder.numFgSeq)/finder.numBgSeq);
+	
+	//cerr<<finder.maxK<<endl;
+	
+	return 0;	
+}
